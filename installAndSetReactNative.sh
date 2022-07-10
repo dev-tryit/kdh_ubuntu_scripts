2b708d9a3361085098fb0be7e8b02fc9ebeed970
TryIt (김동현) <imkim1893@naver.com>
2022-07-10 14:37:07 +0900

    Create installAndSetReactNative.sh

---
+++ installAndSetReactNative.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+nvmPath=https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh
+sudo curl -o- $nvmPath | bash
+
+source ~/.profile
+source ~/.bashrc
+
+nvm install --lts
+nvm use --lts
+
+npm install -g typescript
+npm install -g ts-node
